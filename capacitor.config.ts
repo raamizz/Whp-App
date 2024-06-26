0fd6e5012e475d6c2cf1abc3fd1521037b647064
raamizz <rameezebrahim@transpost.co>
2024-06-26 17:40:03 +0530

    config change

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,9 @@
-import { CapacitorConfig } from "@capacitor/cli";
+import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: "whp.app",
-  appName: "WHP",
-  webDir: "public",
-  server: {
-    url: "http://localhost:3000",
-    cleartext: true,
-  },
+  appId: 'whp.app',
+  appName: 'WHP',
+  webDir: 'build'
 };
 
 export default config;
